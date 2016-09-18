2d1c32cf2ea6e1e4c79ce6ce435e3cf722901458
Sandor Antal Nagy <nagysandorantal@gmail.com>
2016-09-18 19:38:24 +0200

    TypeScript based tests running

---
+++ app/tests/first.spec.ts
@@ -0,0 +1,8 @@
+
+///<reference path="../../node_modules/definitely-typed-jasmine/jasmine.d.ts"/>
+
+"use strict";
+
+describe('1st tests', () => {
+    it('true is true', () => expect(true).toBe(true));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,20 @@
 
 var gulp = require( "gulp" );
 var jshint = require( "gulp-jshint" );
+var karma = require('karma').Server;
 
 gulp.task( "default", function () {
 
     return gulp.src( "./app/scripts/**/*.js" )
         .pipe( jshint() )
         .pipe( jshint.reporter( "default" ) );
-
-    console.log( "Hello gulp" );
 } );
+
+
+gulp.task('test', function (done) {
+    new karma({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
